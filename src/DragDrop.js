76a67908316e781c6109ffe49d6d1241627f13de
Guilherme Ferreira <360guilherme@gmail.com>
2017-06-02 18:04:47 -0300

    Dragula added

---
+++ src/DragDrop.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Dragula from 'react-dragula';
+
+ 
+
+class DragDrop extends Component {
+  render () {
+    return <div className='container' ref={this.dragulaDecorator}>
+      <div>Swap me around</div>
+      <div>Swap her around</div>
+      <div>Swap him around</div>
+      <div>Swap them around</div>
+      <div>Swap us around</div>
+      <div>Swap things around</div>
+      <div>Swap everything around</div>
+    </div>;
+  }
+  dragulaDecorator = (componentBackingInstance) => {
+    if (componentBackingInstance) {
+      let options = { };
+      Dragula([componentBackingInstance], options);
+    }
+  };
+}
+
+export default DragDrop;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Dragula from 'react-dragula';
 import App from './App';
+import DragDrop from './DragDrop'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<DragDrop />, document.getElementById('dragula'));
 registerServiceWorker();
